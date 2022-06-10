05bbe61a3926126cbddfc5b20ae78ffa1e0097a9
Augusto Iphar <87936004+Augustofrx@users.noreply.github.com>
2022-06-10 11:12:59 -0300

    fix:scroll

--- src/Components/utils/utils.jsx
+++ src/Components/utils/utils.jsx
@@ -1,54 +1,77 @@
+export const handleScrollHome = () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
 
-  export const handleScrollHome = () => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-      })
-  }
-
-  export const handleScrollAbout = () => {
-  if(window.innerWidth <= 450) {
+export const handleScrollAbout = () => {
+  if (window.innerWidth <= 450) {
     window.scrollTo({
       top: 720,
       left: 0,
       behavior: "smooth",
-    })
+    });
   } else if (window.innerWidth >= 1920) {
     window.scrollTo({
       top: 915,
       left: 0,
       behavior: "smooth",
-    })
-  } else if (window.innerWidth >= 1366){
+    });
+  } else if (window.innerWidth >= 1366) {
     window.scrollTo({
       top: 650,
       left: 0,
       behavior: "smooth",
-    })
-  } 
+    });
   }
+};
 
-  export const handleScrollSkills = () => {
+export const handleScrollSkills = () => {
+  if (window.innerWidth >= 1920) {
+    window.scrollTo({
+      top: 1907,
+      left: 0,
+      behavior: "smooth",
+    });
+  } else {
     window.scrollTo({
       top: 1330,
       left: 0,
       behavior: "smooth",
-    })
+    });
   }
+};
 
-  export const handleScrollProyects = () => {
+export const handleScrollProyects = () => {
+  if (window.innerWidth >= 1920) {
+    window.scrollTo({
+      top: 2822,
+      left: 0,
+      behavior: "smooth",
+    });
+  } else {
     window.scrollTo({
       top: 1970,
       left: 0,
       behavior: "smooth",
-  })
+    });
   }
+};
 
-  export const handleScrollContact = () => {
+export const handleScrollContact = () => {
+  if (window.innerWidth >= 1920) {
+    window.scrollTo({
+      top: 3644,
+      left: 0,
+      behavior: "smooth",
+    });
+  } else {
     window.scrollTo({
       top: 2700,
       left: 0,
       behavior: "smooth",
-  })
-}
+    });
+  }
+};
